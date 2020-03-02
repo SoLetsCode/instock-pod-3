d418bd6898cbb93872fc37af50d85d403a3f9d5a
Derek <48116904+SoLetsCode@users.noreply.github.com>
2020-03-02 13:59:36 -0800

    front end template complete with assets, fonts, basic css and folder structure

---
+++ sprint-one/client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import axios from "axios";
+import "./styles/App.css";
+
+class App extends Component {
+  render() {
+    return <div className="App">Instock</div>;
+  }
+}
+
+export default App;
