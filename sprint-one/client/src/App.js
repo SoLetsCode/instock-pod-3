ab457cf46d3bf2daa8128895f5d38f1ad2ff7084
Brian Alves <brianalves801@gmail.com>
2020-03-03 14:28:32 -0800

    deleted duplicate router property

--- sprint-one/client/src/App.js
+++ sprint-one/client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-// import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-// import axios from "axios";
+import axios from "axios";
 import "./styles/App.css";
 import Header from "./components/Header";
 import {
