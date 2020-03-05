d9bd92073a8b97d2e8e20bc4647aa12901d74c5c
Derek <48116904+SoLetsCode@users.noreply.github.com>
2020-03-05 12:13:06 -0800

    changed inventory and location data strucutre and made it work with inventory

--- sprint-one/client/src/components/Inventory.js
+++ sprint-one/client/src/components/Inventory.js
@@ -7,72 +7,64 @@ import kebabImg from "../assets/icons/SVG/icon-kebab-default.svg";
 import addImg from "../assets/icons/SVG/icon-add.svg";
 
 function Inventory({ inventory, location }) {
-  const findProducts = () => {
-    //double for loop to look for products in each warehouse.
+  const findWarehouse = () => {
+    //double for loop to find which products exist at which warehouse
     let tempList = [];
-    for (let each in inventory) {
-      for (let warehouse in location) {
-        if (
-          location[warehouse].product.hasOwnProperty([inventory[each].name])
-        ) {
-          //if the product is found, we will create the card or row with information
-
-          tempList.push(
-            <div className="inventory__product-card" key={uuidv4()}>
-              <div className="inventory__left-container">
-                <div className="inventory__container">
-                  <p className="inventory__label">ITEM</p>
-                  <div className="inventory__name-description-container">
-                    <p className="inventory__name">{inventory[each].name}</p>
-                    <p className="inventory__description">
-                      {inventory[each].description}
-                    </p>
-                  </div>
-                </div>
-
-                <div className="inventory__container">
-                  <p className="inventory__label">LAST ORDERED</p>
-                  <p className="inventory__description">12/12/2018</p>
-                </div>
-
-                <div className="inventory__container">
-                  <p className="inventory__label">LOCATION</p>
+    for (let index in inventory) {
+      for (let warehouseName in inventory[index].product) {
+        tempList.push(
+          <div className="inventory__product-card" key={uuidv4()}>
+            <div className="inventory__left-container">
+              <div className="inventory__container">
+                <p className="inventory__label">ITEM</p>
+                <div className="inventory__name-description-container">
+                  <p className="inventory__name">{inventory[index].name}</p>
                   <p className="inventory__description">
-                    {`${location[warehouse].city}, ${location[warehouse].province}`}
-                  </p>
-                </div>
-
-                <div className="inventory__container">
-                  <p className="inventory__label">QUANTITY</p>
-                  <p className="inventory__description">
-                    {location[warehouse].product[inventory[each].name]}
-                  </p>
-                </div>
-
-                <div className="inventory__container">
-                  <p className="inventory__label">STATUS</p>
-                  <p className="inventory__description">
-                    {location[warehouse].product[inventory[each].name] > 0
-                      ? "In Stock"
-                      : "Out of Stock"}
+                    {inventory[index].product.description}
                   </p>
                 </div>
               </div>
-              <div className="inventory__kebab-remove-container">
-                <img src={kebabImg} alt="kebab" className="inventory__kebab" />
-                <div className="inventory__remove" name={inventory[each].name}>
-                  Remove
-                </div>
+
+              <div className="inventory__container">
+                <p className="inventory__label">LAST ORDERED</p>
+                <p className="inventory__description">12/12/2018</p>
+              </div>
+
+              <div className="inventory__container">
+                <p className="inventory__label">LOCATION</p>
+                <p className="inventory__description">{`${location[warehouseName].city}, ${location[warehouseName].province}`}</p>
+              </div>
+
+              <div className="inventory__container">
+                <p className="inventory__label">QUANTITY</p>
+                <p className="inventory__description">
+                  {inventory[index].product[warehouseName]}
+                </p>
+              </div>
+
+              <div className="inventory__container">
+                <p className="inventory__label">STATUS</p>
+                <p className="inventory__description">
+                  {inventory[index].product[warehouseName] > 0
+                    ? "In Stock"
+                    : "Out of Stock"}
+                </p>
               </div>
             </div>
-          );
-        }
+            <div className="inventory__kebab-remove-container">
+              <img src={kebabImg} alt="kebab" className="inventory__kebab" />
+              <div className="inventory__remove" name={inventory[index].name}>
+                Remove
+              </div>
+            </div>
+          </div>
+        );
       }
     }
     return tempList;
   };
 
-  const inventoryList = findProducts();
+  const inventoryList = findWarehouse();
 
   return (
     <div className="inventory">
