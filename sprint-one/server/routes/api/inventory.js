b9107327062ac1b9b1d29916bd1a3ef8d09f5b01
Derek <48116904+SoLetsCode@users.noreply.github.com>
2020-03-05 12:57:58 -0800

    changed git ignore and inventory.js

--- sprint-one/server/routes/api/inventory.js
+++ sprint-one/server/routes/api/inventory.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 
 const productListFile = __dirname + "/../../models/inventory.json";
 const productList = require(productListFile);
+
+function writeJSONFile(filename, content) {
+  //use this to write to the JSON file when adding or deleting items
+  console.log(filename, content);
+  fs.writeFileSync(filename, JSON.stringify(content), "utf8", err => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  console.log(`changes saved to file ${filename}....`);
+}
 
 router.get("/", (req, res) => {
   res.send(productList);
