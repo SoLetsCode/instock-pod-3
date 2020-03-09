825121141be260f08d045ff99ebf656c3917ef3d
Asewid <asewid@gmail.com>
2020-03-08 21:17:57 -0700

    Backing up products component

--- sprint-one/client/src/components/Products.js
+++ sprint-one/client/src/components/Products.js
@@ -11,49 +11,85 @@ const Products = () => {
         <button className="products__header-instock-btn">In Stock</button>
       </div>
 
-      <div className="products__segment">
-        <h2>ITEM DESCRIPTION</h2>
-        <p>
+      <div className="products__segment-uno">
+        <h2 className="products__segment-flex-item products__segment-title">
+          ITEM DESCRIPTION
+        </h2>
+        <p className="products__segment-flex-item products__segment-content">
           Here is a more detailed summary of the product name, it's uses,
           industries and possible attributes that could be used to describe the
           product.
         </p>
       </div>
+      <div>
+        <div className="products__segment">
+          <div className="products__segment-section">
+            <h2 className="products__segment-flex-item products__segment-title">
+              ORDERED BY
+            </h2>
+            <p className="products__segment-flex-item products__segment-content">
+              Mark Sauders
+            </p>
+          </div>
 
-      <div className="products__segment">
-        <div className="">
-          <h2>ORDERED BY</h2>
-          <p>Mark Sauders</p>
+          <div className="products__segment-section">
+            <h2 className="products__segment-flex-item products__segment-title">
+              REFERENCED NUMBER
+            </h2>
+            <p className="products__segment-flex-item products__segment-content">
+              JK202OFD7811201
+            </p>
+          </div>
         </div>
-        <div className="">
-          <h2>REFERENCED NUMBER</h2>
-          <p>JK202OFD7811201</p>
-        </div>
-      </div>
 
-      <div className="products__segment">
-        <div className="div">
-          <h2>LAST ORDERED</h2>
-          <p>5/24/2018</p>
-        </div>
-        <div className="">
-          <h2>LOCATION</h2>
-          <p>Toronto, ON</p>
-        </div>
-      </div>
+        <div className="products__segment">
+          <div className="products__segment-section">
+            <h2 className="products__segment-flex-item products__segment-title">
+              LAST ORDERED
+            </h2>
+            <p className="products__segment-flex-item products__segment-content">
+              5/24/2018
+            </p>
+          </div>
 
-      <div className="">
-        <h2>QUANTITY</h2>
-        <p>12,000</p>
+          <div className="products__segment-section">
+            <h2 className="products__segment-flex-item products__segment-title">
+              LOCATION
+            </h2>
+            <p className="products__segment-flex-item products__segment-content">
+              Toronto, ON
+            </p>
+          </div>
+        </div>
+
+        <div className="products__segment-uno products__segment-final">
+          <div className="products__segment-section">
+            <h2 className="products__segment-flex-item products__segment-title">
+              QUANTITY
+            </h2>
+            <p className="products__segment-flex-item products__segment-content">
+              12,000
+            </p>
+          </div>
+        </div>
+        <div className="products__segment-uno">
+          <div className="products__segment-section">
+            <h2 className="products__segment-flex-item products__segment-title">
+              CATEGORIES
+            </h2>
+            <p className="products__segment-flex-item products__segment-content">
+              Industrial, Automotive, Heavy, Mechancial, Engineering,
+              Transporation, Sales
+            </p>
+          </div>
+          <div className="products__segment-section">
+            <div>&nbsp;</div>
+          </div>
+        </div>
       </div>
-      <div className="">
-        <h2>CATEGORIES</h2>
-        <p>
-          Industrial, Automotive, Heavy, Mechancial, Engineering, Transporation,
-          Sales
-        </p>
+      <div className="products__edit-btn-wrapper">
+        <button className="products__edit-btn">EDIT</button>
       </div>
-      <button>EDIT</button>
     </section>
   );
 };
